f6c95e1c132ac64d929a66e2ab0e2e6235e0688c
Олег <rahilov@gmail.com>
2026-02-02 17:07:55 +0300

    fix: add shebang and safety check to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install && psql -a -d $DATABASE_URL -f database.sql
+make install
+if [ -n "$DATABASE_URL" ]; then
+  psql -a -d "$DATABASE_URL" -f database.sql
+fi
